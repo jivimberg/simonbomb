292c2f7e18fdf2eb90dcf662d1aa6e3fd9136d04
Juan Ignacio Vimberg <jivimberg@gmail.com>
2015-12-10 08:55:09 -0800

    Timer running, first version

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -26,8 +26,16 @@ angular.module('simonbombApp')
     };
 
     $scope.newGame = function() {
-      // push a message to the end of the array
+      // clean the sequence
       Ref.child('simonSequence').remove();
+
+      Ref.child('.info/serverTimeOffset').on('value', function(snap) {
+        myOffset = snap.val()||0;
+      });
+      Ref.child('running').on('value', toggleRunning);
+      Ref.child('endtime').on('value', updateEndTime);
+      Ref.child('endtime').set(now() + RESET_SECONDS * 1000);
+      Ref.child('running').set(true);
     };
 
     function alert(msg) {
@@ -36,4 +44,51 @@ angular.module('simonbombApp')
         $scope.err = null;
       }, 5000);
     }
+
+    /***** TIMER FUNCTIONS *********/
+    var endsAt = 0, timeout;
+    var RESET_SECONDS = 60;
+    var myOffset = 0;
+
+    function toggleRunning(snap) {
+      var b = !! snap.val();
+      if (b) {
+        countDown();
+        timeout = setInterval(countDown, 1000);
+      } else {
+        timeout && clearTimeout(timeout);
+        timeout = null;
+      }
+    }
+
+    function updateEndTime(snap) {
+      endsAt = snap.val()||0;
+      countDown();
+    }
+
+    function countDown() {
+      setTime(Math.max(0, endsAt - now()));
+    }
+
+    function setTime(remaining) {
+      var minutes = Math.floor(remaining / 60000);
+      var seconds = Math.round(remaining / 1000);
+      $('#timer').text(lpad(minutes) + ':' + lpad(seconds));
+    }
+
+    /******* UTILS **********/
+
+    function now() {
+      return Date.now() + myOffset;
+    }
+
+    function lpad(n) {
+      if (n < 10) {
+        return '0' + n;
+      }
+      else {
+        return n;
+      }
+    }
+
   });
