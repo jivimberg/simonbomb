d9c552786ccc6c4a11098ff3253c733abdb00a41
Juan Ignacio Vimberg <jivimberg@gmail.com>
2015-12-09 22:47:11 -0800

    initial code drop

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name simonbombApp
+ * @description
+ * # simonbombApp
+ *
+ * Main module of the application.
+ */
+angular.module('simonbombApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
